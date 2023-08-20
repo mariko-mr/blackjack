08ba51ada886a6aa6ea88b0220a4e67c6c4393cf
mariko_mr <ma.kakiuchi@gmail.com>
2023-08-20 14:31:47 +0900

    $winnerMsg 変数の削除

--- src/lib/HandJudger.php
+++ src/lib/HandJudger.php
@@ -6,20 +6,16 @@ class HandJudger
 {
     public function determineWinner(int $playerTotalScore, int $dealerTotalScore): string
     {
-        $winnerMsg = '';
-
         if ($dealerTotalScore > 21) {
-            $winnerMsg = 'ディーラーはバーストしました。あなたの勝ちです！';
+            return 'ディーラーはバーストしました。あなたの勝ちです！';
         }
 
         if ($dealerTotalScore === $playerTotalScore) {
-            $winnerMsg = '同点でした。この勝負は引き分けとします。';
+            return '同点でした。この勝負は引き分けとします。';
         } elseif ($playerTotalScore > $dealerTotalScore) {
-            $winnerMsg = 'あなたの勝ちです！';
+            return 'あなたの勝ちです！';
         } elseif ($playerTotalScore < $dealerTotalScore) {
-            $winnerMsg = 'ディーラーの勝ちです。残念！';
+            return 'ディーラーの勝ちです。残念！';
         }
-
-        return $winnerMsg;
     }
 }
